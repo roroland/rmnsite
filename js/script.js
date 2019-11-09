a3aa0ba2c863db57d98017e3e523ea76b6cbc238
roroland <roroland@gmail.com>
2019-11-09 19:09:39 -0300

    improve interactions

--- js/script.js
+++ js/script.js
@@ -1,32 +1,35 @@
 const itemNav = document.querySelectorAll('.itemNav li a');
 const showFeature = document.querySelector('.showFeature');
-const closeFeature = document.getElementById('close');
 let itemNavId = '';
+let closeFeature = '';
 
-closeFeature.onclick = close;
-function getFeature() {
-  showFeature.classList.add('show', 'show-' + itemNavId);
-}
 itemNav.forEach(function (item) {
   item.onclick = function (e) {
     e.preventDefault();
     itemNavId = this.getAttribute('id');
     getFeature();
-    move();
   }
 });
 
-function close(e) {
-  e.preventDefault();
-  showFeature.classList.remove('show', 'show-' + itemNavId);
-  itemNavId = '';
+function getFeature() {
+  showFeature.classList.add('show', 'show-' + itemNavId);
+  closeFeature = document.querySelector('.contentItem-' + itemNavId + ' .close');
+  closeFeature.addEventListener('click', close);
+  move();
 }
+
 function move() {
   let root = document.documentElement;
-  let placeholder = document.querySelector('.contentItem-text');
-  let listener = placeholder.addEventListener(('mouseenter', 'mousemove'), e => {
-    root.style.setProperty('--xpos', -e.clientX + (placeholder.offsetHeight/2) + "px");
-    root.style.setProperty('--ypos', -e.clientY + (placeholder.offsetHeight/2) + "px");
+  let placeholder = document.querySelector('.contentItem-' + itemNavId + ' .contentItem-text');
+  let listener = placeholder.addEventListener(('touchstart', 'touchmove', 'mouseenter', 'mousemove'), e => {
+    root.style.setProperty('--xpos', -e.clientX + (placeholder.offsetHeight / 2) + "px");
+    root.style.setProperty('--ypos', -e.clientY + (placeholder.offsetHeight / 2) + "px");
   })
-  placeholder.removeEventListener(('mouseout', 'mouseleave'), listener);
+  placeholder.removeEventListener(('touchend', 'touchleave', 'mouseout', 'mouseleave'), listener);
+}
+
+function close(e) {
+  e.preventDefault();
+    showFeature.classList.remove('show', 'show-' + itemNavId);
+    itemNavId = '';
 }

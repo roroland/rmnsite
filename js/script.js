73ef1aaa1b24da81c57364a4ed0270c681b7510c
roroland <roroland@gmail.com>
2019-11-16 14:54:58 -0300

    Improve script

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ const showFeature = document.querySelector('.showFeature');
 let itemNavId = '';
 let closeFeature = '';
 let cover = '';
+let root = document.documentElement;
+let initX = root.style.setProperty('--xpos', 150);
+let initY = root.style.setProperty('--ypos', 350);
 
 itemNav.forEach(function (item) {
   item.onclick = function (e) {
@@ -22,7 +25,6 @@ function getFeature() {
 }
 
 function move() {
-  let root = document.documentElement;
   let placeholder = document.querySelector('.contentItem-' + itemNavId + ' .contentItem-text');
   let listener = placeholder.addEventListener(('touchstart', 'touchmove', 'mouseenter', 'mousemove'), e => {
     root.style.setProperty('--xpos', -e.clientX + (placeholder.offsetHeight / 2) + "px");
@@ -30,14 +32,17 @@ function move() {
   })
   placeholder.removeEventListener(('touchend', 'touchleave', 'mouseout', 'mouseleave'), listener);
 }
+
 function coverOut() {
   cover.classList.add('is-active');
 }
+
 function close(e) {
   e.preventDefault();
   showFeature.classList.remove('show', 'show-' + itemNavId);
   itemNavId = '';
   cover.classList.remove('is-active');
+  initX = root.style.setProperty('--xpos', 150);
+  initY = root.style.setProperty('--ypos', 350);
   clearTimeout(coverOut);
 }
-
